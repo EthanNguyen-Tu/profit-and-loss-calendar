7d54ce474609d4a67e602766b3cee9e16099cd77
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-10 04:01:54 -0400

    feat: add component to export calendar data

---
+++ src/components/ExportDataButton/ExportDataButton.tsx
@@ -0,0 +1,34 @@
+import useCalendar from "@/hooks/useCalendar";
+import { Download } from "lucide-react";
+import IconButton from "@/components/IconButton/IconButton";
+
+export default function ExportDataButton() {
+    const { exportData } = useCalendar();
+
+    const handleExport = () => {
+        const data = JSON.stringify(exportData(), null, 2);
+        const dataBlob = new Blob([data], { type: "application/json" });
+        const url = URL.createObjectURL(dataBlob);
+        const link = document.createElement("a");
+
+        link.href = url;
+        link.download = `pl-calendar-data-${
+            new Date().toISOString().split("T")[0]
+        }.json`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+    };
+
+    return (
+        <IconButton
+            icon={<Download size={16} />}
+            onClick={handleExport}
+            title="Export Calendar Data"
+            ariaLabel={"Export calendar data button"}
+        >
+            Export
+        </IconButton>
+    );
+}
