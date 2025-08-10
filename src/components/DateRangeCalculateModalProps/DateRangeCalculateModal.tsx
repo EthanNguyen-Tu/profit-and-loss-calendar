a65e2ac8dcc0ca4ab39d9773b708bbc82bc75aab
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-09 22:10:27 -0400

    feat: add modal for calculate profit or loss within a date range

---
+++ src/components/DateRangeCalculateModalProps/DateRangeCalculateModal.tsx
@@ -0,0 +1,83 @@
+import useCalendar from "@/hooks/useCalendar";
+import { formatUSD, getCurrencyColorClass } from "@/lib/currency";
+import { Calculator } from "lucide-react";
+import { useState } from "react";
+import IconButton from "../IconButton/IconButton";
+import Modal from "../Modal/Modal";
+
+interface DateRangeCalculateModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export default function DateRangeCalculateModal({
+    isOpen,
+    onClose,
+}: DateRangeCalculateModalProps) {
+    const { calculateRangePL } = useCalendar();
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [result, setResult] = useState<number | null>(null);
+
+    const handleCalculate = () => {
+        if (startDate && endDate) {
+            setResult(calculateRangePL(startDate, endDate));
+        }
+    };
+
+    const handleClose = () => {
+        setStartDate("");
+        setEndDate("");
+        setResult(null);
+        onClose();
+    };
+
+    if (!isOpen) return null;
+
+    return (
+        <Modal
+            title="Calculate P&L Range"
+            isOpen={isOpen}
+            onClose={handleClose}
+            ctas={
+                <IconButton
+                    icon={<Calculator size={16} />}
+                    onClick={handleCalculate}
+                    disabled={!startDate || !endDate}
+                    title="Calculate P&L Range"
+                    ariaLabel="Calculate P&L Range"
+                    style={{ alignSelf: "flex-end" }}
+                >
+                    Calculate
+                </IconButton>
+            }
+        >
+            <div className="flex-column">
+                <div className="inputEntry">
+                    <label>Start Date</label>
+                    <input
+                        type="date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                    />
+                </div>
+                <div className="inputEntry">
+                    <label>End Date</label>
+                    <input
+                        type="date"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)}
+                    />
+                </div>
+                <div className="inputEntry">
+                    Result:{" "}
+                    {result && (
+                        <div className={getCurrencyColorClass(result)}>
+                            {formatUSD(result)}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </Modal>
+    );
+}
