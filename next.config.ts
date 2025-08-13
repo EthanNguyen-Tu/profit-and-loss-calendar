1a9984b917a39a90acbd6c783462f1128a98b531
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-13 03:32:31 -0400

    chore: update Next.js config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "/profit-and-loss-calendar",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
+
