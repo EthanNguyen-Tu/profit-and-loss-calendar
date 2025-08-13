e53c56af0add27677d5dc1cd32f59bea98f0c9c1
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-08-13 03:17:55 -0400

    feat: add about button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Calendar } from "@/components/Calendar/Calendar";
 import CalendarProvider from "@/providers/CalendarProvider";
 import styles from "./page.module.css";
 import { PLChart } from "@/components/PLChart/PLChart";
+import HelpButton from "@/components/HelpButton/HelpButton";
+import AboutButton from "@/components/AboutButton/AboutButton";
 
 export default function Home() {
     return (
@@ -12,6 +14,8 @@ export default function Home() {
                 <div className={styles.header}>
                     <h1>P&L Calendar</h1>
                     <p>By Ethan Nguyen-Tu</p>
+                    <AboutButton />
+                    <HelpButton />
                 </div>
                 <Calendar />
                 <PLChart />
---
+++ src/components/AboutButton/AboutButton.tsx
@@ -0,0 +1,63 @@
+import Modal from "@/components/Modal/Modal";
+import { Info } from "lucide-react";
+import IconButton from "@/components/IconButton/IconButton";
+import { useState } from "react";
+
+export default function AboutButton() {
+    const [showAboutModal, setShowAboutModal] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setShowAboutModal(!showAboutModal);
+    };
+
+    const handleClose = () => {
+        setShowAboutModal(false);
+    };
+
+    return (
+        <>
+            <IconButton
+                icon={<Info />}
+                onClick={handleClick}
+                title={"About"}
+                style={{
+                    position: "fixed",
+                    top: "1rem",
+                    right: "3rem",
+                    zIndex: 999,
+                }}
+                variant="tertiary"
+            />
+            <Modal
+                isOpen={showAboutModal}
+                onClose={handleClose}
+                title="About"
+                style={{
+                    maxWidth: "90%",
+                    minWidth: "350px",
+                    alignItems: "flex-start",
+                    fontSize: "0.8rem",
+                    zIndex: 999,
+                }}
+            >
+                <p>
+                    Made by Ethan Nguyen-Tu, this application is designed to
+                    help you track your daily profit and loss by providing a
+                    simple and intuitive interface for entering and viewing your
+                    financial data.
+                </p>
+                <p>
+                    All inputted data is stored locally in your browser&apos;s
+                    storage, ensuring your information is kept private and
+                    secure. You can easily export your data as a JSON file by
+                    clicking the &quot;Export&quot; button at the top of the
+                    calendar to save your changes. This exported data can then
+                    be imported back into the application by clicking the
+                    &quot;Import&quot; button when you next open the calendar,
+                    allowing you to restore your changes.
+                </p>
+                <p>Copyright © 2025 Ethan Nguyen-Tu. All rights reserved.</p>
+            </Modal>
+        </>
+    );
+}
